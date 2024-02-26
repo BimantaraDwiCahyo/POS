7dcd8f26325851907d73e7a42c66c8d97a977088
BimantaraDwiCahyo <27tapai@gmail.com>
2024-02-26 21:28:27 +0700

    Studi kasus

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ProductsController extends Controller
+{
+    public function foodBeverage()
+    {
+        return view('products.food_beverage');
+    }
+
+    public function beautyHealth()
+    {
+        return view('products.beauty_health');
+    }
+
+    public function homeCare()
+    {
+        return view('products.home_care');
+    }
+
+    public function babyKid()
+    {
+        return view('products.baby_kid');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,8 +14,21 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::prefix('products')->group(function () {
+    Route::get('/category/food-beverage', [ProductsController::class, 'foodBeverage'])->name('products.food_beverage');
+    Route::get('/category/beauty-health', [ProductsController::class, 'beautyHealth'])->name('products.beauty_health');
+    Route::get('/category/home-care', [ProductsController::class, 'homeCare'])->name('products.home_care');
+    Route::get('/category/baby-kid', [ProductsController::class, 'babyKid'])->name('products.baby_kid');
 });
+
+Route::get('/user/{id}/name/{name}', [UserController::class, 'show'])->name('user.show');
+
+Route::get('/sales', [SalesController::class, 'index'])->name('sales.index');
